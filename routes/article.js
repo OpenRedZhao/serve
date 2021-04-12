daf2d8752dc1f85f0c370b6f0af26e8aae843999
OpenRedZhao <1719449113@qq.com>
2021-04-12 10:21:11 +0800

    修改新增接口

--- routes/article.js
+++ routes/article.js
@@ -4,13 +4,13 @@ const querySql = require('../db/index')
 
 /* 新增博客接口 */
 router.post('/add', async(req, res, next) => {
-  let {title, content} = req.body
+  let {title, content, video_url} = req.body
   let {username} = req.user
   try {
     let result = await querySql('select id from user where  username = ?', [username])
     let user_id = result[0].id
     console.log(user_id)
-    await querySql('insert into article(title,content,user_id,create_time)values(?,?,?,NOW())',[title,content,user_id])
+    await querySql('insert into article(title,content,video_url,user_id,create_time)values(?,?,?,?,NOW())',[title,content,video_url,user_id])
     res.send({code:0,msg:'新增成功',data:null})
   }catch(e){
     console.log(e)
